1b40b154b7a160c16a27a2848b2fdd3523d4a82a
dgerth5 <davidgerth5@gmail.com>
2024-07-22 20:26:25 -0500

    update defaults

--- ui.R
+++ ui.R
@@ -16,10 +16,10 @@ ui <- fluidPage(
   
   fluidRow(
     column(1, div(style = "margin-top: 25px;", strong("Pitcher 2:"))),
-    column(2, numericInput("Age_Row2", "Age", value = 30)),
-    column(2, numericInput("IP_Row2", "IP", value = 120)),
-    column(2, numericInput("k_Row2", "K%+", value = 90)),
-    column(2, numericInput("b_Row2", "BB%+", value = 25)),
+    column(2, numericInput("Age_Row2", "Age", value = 25)),
+    column(2, numericInput("IP_Row2", "IP", value = 100)),
+    column(2, numericInput("k_Row2", "K%+", value = 100)),
+    column(2, numericInput("b_Row2", "BB%+", value = 100)),
     column(2, selectInput("starter_Row2", "Starter?", choices = c("Yes" = 1, "No" = 0), selected = 1))
   ),
   
